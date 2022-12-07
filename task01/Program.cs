bbfaa14f6c303e90c4f15ffd313f31d5c7314e2e
LadaSPB <LadaSpb@yandex.ru>
2022-12-08 00:14:52 +0300

    задача

---
+++ task01/Program.cs
@@ -0,0 +1,30 @@
+﻿void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите " + i + " элемент массива: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+}
+void NewArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+            Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите первоначальный размер элементов массива");
+int massif = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[massif];
+InputArray(array);
+Console.WriteLine();
+Console.WriteLine($"Введённый первоначальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("Полученный массив: ");
+NewArray(array);
+Console.ResetColor();
